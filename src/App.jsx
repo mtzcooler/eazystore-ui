d73afa59ff5faefbecee13c504c816eb787b6e4d
Matheus Zoccoler <mtzcooler@gmail.com>
2025-07-08 17:42:08 +0100

    Show page heading and title using props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 import ClassHeader from "./components/ClassHeader";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import PageHeading from "./PageHeading";
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <PageHeading title="Explore Eazy Stickers!">
+                Add a touch of creativity to your space with our wide range of fun and unique stickers.
+                Whether you're looking to personalize your laptop, decorate your room, or find the perfect gift, we have it.
+                Browse our collection and let your imagination run wild!
+            </PageHeading>
+        </div>
+    );
+}
