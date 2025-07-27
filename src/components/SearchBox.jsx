387bb21d10e73add76d05661488037b6b479c217
Matheus <mtzcooler@gmail.com>
2025-07-27 20:50:55 +0100

    Search box using React Hooks

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function SearchBox({ label, placeholder, value, handleSearch }) {
+
+  return (
+    <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
+      <label className="text-lg font-semibold text-primary">{label}</label>
+      <input
+        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => handleSearch(event.target.value)}
+      />
+    </div>
+  );
+}
--- src/components/product/ProductListing.jsx
+++ src/components/product/ProductListing.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
+import SearchBox from "../SearchBox";
+import Dropdown from "../Dropdown";
+
+const sortList = ["Popularity", "Price: Low to High", "Price: High to Low"];
 
 export default function ProductListing({ products }) {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchChange(value) {
+    setSearchText(value);
+  }
+
+  let filteredSortedProducts = Array.isArray(products)
+    ? products.filter((product) =>
+        product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchText.toLowerCase())
+      )
+    : [];
+
   return (
     <div className="max-w-[1152px] mx-auto">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox
+          label="Search"
+          placeholder="Search products..."
+          value={searchText}
+          handleSearch={(value) => handleSearchChange(value)}
+        />
+        <Dropdown
+          label="Sort by"
+          options={sortList}
+          selectedValue="Popularity"
+        />
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {filteredSortedProducts.length > 0 ? (
+          filteredSortedProducts.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))
         ) : (
-          <p className="text-center font-primary font-bold text-lg text-primary">No products found.</p>
+          <p className="text-center font-primary font-bold text-lg text-primary">
+            No products found.
+          </p>
         )}
       </div>
     </div>
