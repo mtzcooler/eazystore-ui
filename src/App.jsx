b211a0787b68c440beee7f8224941222d8b1783a
Matheus <mtzcooler@gmail.com>
2025-07-30 12:43:51 +0100

    Maintain header and footer across pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </>
   );
