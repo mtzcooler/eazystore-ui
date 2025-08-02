13e88132bede21715700a03c627be1f49e952928
Matheus <mtzcooler@gmail.com>
2025-08-02 12:44:05 +0100

    Add Product Detail page with zooming function

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Login from "./components/Login.jsx";
 import Cart from "./components/Cart.jsx";
+import ProductDetail from "./components/product/ProductDetail.jsx";
 import {
   createRoutesFromElements,
   Route,
@@ -27,6 +28,7 @@ const routeDefinitions = createRoutesFromElements(
     <Route path="/contact" element={<Contact />} action={contactAction} />
     <Route path="/login" element={<Login />} />
     <Route path="/cart" element={<Cart />} />
+    <Route path="/products/:productId" element={<ProductDetail />} />
   </Route>
 );
 
